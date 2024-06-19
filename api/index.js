29d4e197b1e798f3035ccbb01691ad1964c29c22
Joy Brar <joybrar2001@gmail.com>
2024-06-19 12:01:12 +0530

    Ambassador

--- api/index.js
+++ api/index.js
@@ -27,9 +27,9 @@ ssh.connect({
 function runScript(filename, callback) {
   let scriptPath;
   if (filename === 'UnlockScript') {
-    scriptPath = '/home/vanshaj_arora_sg/PinUS.py';
+    scriptPath = '/home/vanshaj_arora_sg/PinTabletAmbassador.py';
   } else if (filename === 'morningSetup') {
-    scriptPath = '/home/vanshaj_arora_sg/UnpinUS.py';
+    scriptPath = '/home/vanshaj_arora_sg/UnpinTabletAmbassador.py';
   } else {
     scriptPath = '/home/vanshaj_arora_sg/testlit.py';
   }
